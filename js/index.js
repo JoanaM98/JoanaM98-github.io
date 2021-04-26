51183df73475dd0d7fbc9b4fd2e2f50d1d6e076f
JoanaM98 <75888056+JoanaM98@users.noreply.github.com>
2021-04-26 11:39:13 +0100

    Initial Files

---
+++ js/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+$(".about-link").click(function(){
+  $("html, body").animate({scrollTop:$("#sobre").offset().top},1000);
+})
+$(".education-link").click(function(){
+  $("html, body").animate({scrollTop:$("#educação").offset().top},1000);
+})
+$(".experience-link").click(function(){
+  $("html, body").animate({scrollTop:$("#experiência").offset().top},1000);
+})
+$(".skills-link").click(function(){
+  $("html, body").animate({scrollTop:$("#skills").offset().top},1000);
+})
+
+$(".contacts-link").click(function(){
+  $("html, body").animate({scrollTop:$("#contactos").offset().top},1000);
+})
+
+
+})
